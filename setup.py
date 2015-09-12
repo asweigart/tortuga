64657cfdfc610f1543ba328ee7254746d64fe65b
Al Sweigart <asweigart@gmail.com>
2015-09-12 14:37:06 -0500

    Wrestling with pypi listing.

--- setup.py
+++ setup.py
@@ -4,16 +4,17 @@ from setuptools import setup
 
 setup(
     name='Tortuga',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/asweigart/tortuga',
     author='Al Sweigart',
     author_email='al@inventwithpython.com',
     description=("Una aplicación española del módulo turtle.py de Python. A Spanish implementation of Python's turtle.py module."),
     license='BSD',
     packages=['tortuga'],
+    test_suite='tests',
     keywords="turtle logo spanish",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 3 - Alpha',
         'Environment :: Win32 (MS Windows)',
         'Environment :: X11 Applications',
         'Environment :: MacOS X',
@@ -22,7 +23,6 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
