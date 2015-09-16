00ab6b35cd317e3b3b059801f30e4ddc32f7d09d
Al Sweigart <asweigart@gmail.com>
2015-09-16 15:13:31 -0700

    Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='Tortuga',
-    version='0.0.3',
+    version='0.0.4',
     url='https://github.com/asweigart/tortuga',
     author='Al Sweigart',
     author_email='al@inventwithpython.com',
